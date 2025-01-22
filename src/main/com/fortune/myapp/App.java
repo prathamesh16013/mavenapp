27c7fe5980a1f2c81b989a425daa726512f4b7d8
prathamesh16013 <prathameshpatil16013@gmail.com>
2025-01-22 10:09:16 +0000

    first comiy in ok

---
+++ src/main/com/fortune/myapp/App.java
@@ -0,0 +1,28 @@
+package com.fortune.myapp; 
+public class App { 
+
+ 
+
+    private static final String MESSAGE = "Hello World!"; 
+
+ 
+
+    public App() {} 
+
+ 
+
+    public static void main(String[] args) { 
+
+        System.out.println(MESSAGE); 
+
+    } 
+
+ 
+
+    public String getMessage() { 
+
+        return MESSAGE; 
+
+    } 
+
+} 
---
+++ src/main/com/fortune/myapp/Apptest.java
@@ -0,0 +1,40 @@
+package com.fortune.myapp; 
+
+ 
+
+import org.junit.jupiter.api.Test; 
+
+ 
+
+import static org.junit.jupiter.api.Assertions.assertEquals; 
+public class AppTest 
+
+{ 
+
+    @Test 
+
+    public void testAppConstructor() { 
+
+        App app1 = new App(); 
+
+        App app2 = new App(); 
+
+        assertEquals(app1.getMessage(), app2.getMessage()); 
+
+    } 
+
+ 
+
+    @Test 
+
+    public void testAppMessage() 
+
+    { 
+
+        App app = new App(); 
+
+        assertEquals("Hello World!", app.getMessage()); 
+
+    } 
+
+} 
